02073e857f6d592c0bf4ac2f265b4c8db78ee3e8
Jay St.Gelais <JayStGelais@gmail.com>
2014-05-22 17:27:36 -0400

    Junit Assume FTW!

---
+++ core/src/test/groovy/com/commercehub/dbratchet/schema/redgate/SqlCompareTest.groovy
@@ -0,0 +1,19 @@
+package com.commercehub.dbratchet.schema.redgate
+
+
+import static org.junit.Assume.*
+
+import org.junit.Test
+
+/**
+ * Created by jgelais on 5/22/2014.
+ */
+class SqlCompareTest {
+
+    @Test
+    void testAssumptions() {
+        assumeTrue(false)
+        assert new SqlCompareFactory().newSqlCompare()
+    }
+
+}
